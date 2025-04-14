c9a6d1e6358ae1e7ba7c8df8dd3cd54e82a5c67e
southfreebird <yvorott@gmail.com>
2025-04-14 13:06:25 +0000

    123

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 
-
+import jax
+import kvax
+    
 mesh = 
 attention_specs = 
 flash_attention = 
+mesh_devices = 
+
 
 
 
@@ -11,18 +15,20 @@ flash_attention =
 # kv: (batch, kv_sequence, kv_heads, attention_head_dim)
       
 # Context Parallelism
+mesh = jax.sharding.Mesh(mesh_devices, ("data", "context"))
 with mesh, attention_specs(
     query_specs=("data", "context", None, None),
     kv_specs=("data", None, None, None),
 ):
-    attn_out = flash_attention(...)
+    attn_out = kvax.ops.flash_attention(...)
 
 # Tensor Parallelism
+mesh = jax.sharding.Mesh(mesh_devices, ("data", "model"))
 with mesh, attention_specs(
     query_specs=("data", None, "model", None),
     kv_specs=("data", None, "model", None),
 ):
-    attn_out = flash_attention(...)
+    attn_out = kvax.ops.flash_attention(...)
 
 
 
